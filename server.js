149c9f33c1b890606d16fd2f223f04ee103e6192
tanner <tanner.chell@gmail.com>
2018-12-17 00:05:46 -0700

    much styling done

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 const index = __dirname + '/views/index.html';
 const static_content = __dirname + '/public';
+const bootstrap = __dirname + 'node_modules/bootstrap/dist/css/';
 
 app.use(express.static(static_content));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.get('/', (req, res) => {
     res.sendFile(index);
