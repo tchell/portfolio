5557734e0ce68256553478c5cf7d12ddcf57c3f4
Tanner Chell <tanner.chell@gmail.com>
2018-12-29 17:13:54 -0700

    Added email with nodemailer and Gmail API

--- public/js/submit.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    console.log("hello jQuery");
-    $("#submit").click(function() {
-        $("test-form").post('/',
-            {"message": $("#test").val()}
-        )
-    });
-});
