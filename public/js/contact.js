b4064312fd9a493788b11f417e255cd5ee642cae
Tanner Chell <tanner.chell@gmail.com>
2019-05-28 14:44:27 -0600

    Added reCaptcha validation
    - changed some whitespace...

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,11 +1,23 @@
 
-var onRecaptchaLoad = function() {
-    grecaptcha.ready(function() {
-        grecaptcha.render('recaptcha', { 'sitekey': '6LdvjpkUAAAAAE6HxyvlRUQYjEITWoNzEqy-TZTE'}
+var onRecaptchaLoad = function () {
+    grecaptcha.ready(function () {
+        grecaptcha.render('recaptcha', { 'sitekey': '6LdvjpkUAAAAAE6HxyvlRUQYjEITWoNzEqy-TZTE' }
         );
     })
 }
 
-var recaptchaCompleted = function() {
-    $("#submit").prop("disabled", false);
+var recaptchaCompleted = function (token) {
+    body = { token: token };
+    $.post('/reCaptcha', body, (data, status) => {
+        if (data.success === true) {
+            $("#submit").prop("disabled", false)
+                .addClass("btn-primary");
+        }
+    });
 }
+
+$(document).ready(function () {
+    $("#contact-form").submit(function (event) {
+        grecaptcha.reset();
+    });
+});
